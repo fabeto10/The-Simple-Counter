8e8af86b67a6ed41b0617dbb25f075b771c47e8d
fabeto10 <f0414475@gmail.com>
2022-05-26 21:34:25 +0000

    simplecounter treminado

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,55 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import PropTypes from "prop-types";
 
-//import your own components
-import Home from "./component/home.jsx";
+const SimpleCounter = (props) => {
+	return (
+		<div className="bigCounter">
+			<div style={{ borderRadius: "30px" }} className="calendar">
+				<i className="far fa-clock"></i>
+			</div>
+			<div style={{ borderRadius: "30px" }} className="four">
+				{props.digitFour}
+			</div>
+			<div style={{ borderRadius: "30px" }} className="three">
+				{props.digitThree}
+			</div>
+			<div style={{ borderRadius: "30px" }} className="two">
+				{props.digitTwo}
+			</div>
+			<div style={{ borderRadius: "30px" }} className="one">
+				{props.digitOne}
+			</div>
+		</div>
+	);
+};
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+SimpleCounter.propTypes = {
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number,
+};
+
+let counter = 0;
+
+setInterval(() => {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	console.log(four, three, two, one);
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={one % 10}
+			digitTwo={two % 10}
+			digitThree={three % 10}
+			digitFour={four % 10}
+		/>,
+		document.querySelector("#app")
+	);
+	counter++;
+}, 1000);
